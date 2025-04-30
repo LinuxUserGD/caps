46ba6c9f54d523dcb01ee3e51a554b6d13ff46c2
Linuxuser Gd <hugegameartgd@gmail.com>
2025-05-01 00:09:53 +0200

    add typedefs

--- basics.h
+++ basics.h
@@ -49,6 +49,15 @@
 
 #include "ladspa.h"
 
+typedef int8_t __int8_t;
+typedef int16_t __int16_t;
+typedef int32_t __int32_t;
+typedef int64_t __int64_t;
+typedef uint8_t __uint8_t;
+typedef uint16_t __uint16_t;
+typedef uint32_t __uint32_t;
+typedef uint64_t __uint64_t;
+
 typedef __int8_t			int8;
 typedef __uint8_t			uint8;
 typedef __int16_t			int16;
